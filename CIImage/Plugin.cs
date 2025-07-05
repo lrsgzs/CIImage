2dade01bc6b71e55c1fee0c58ad904b93aa946f4
lrs2187 <liurongshuo2022@outlook.com>
2025-07-05 21:10:44 +0800

    :feat: 初始化项目

---
+++ CIImage/Plugin.cs
@@ -0,0 +1,18 @@
+
+using ClassIsland.Core.Abstractions;
+using ClassIsland.Core.Attributes;
+using ClassIsland.Core.Controls.CommonDialog;
+using ClassIsland.Core.Extensions.Registry;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace CIImage;
+
+[PluginEntrance]
+public class Plugin : PluginBase
+{
+    public override void Initialize(HostBuilderContext context, IServiceCollection services)
+    {
+        
+    }
+}
